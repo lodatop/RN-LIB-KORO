22d2385d3d9f8fb699920cb26057eeefd44a6364
Wisam <wisam_mozalbat@hotmail.com>
2020-03-24 00:32:40 -0400

    Finished the Steps Component

--- App.js
+++ App.js
@@ -76,7 +76,9 @@ export default function App() {
       <KoroDropdown onSelect={(val) => setDs(val)} options={[1,2,3]} />
       <Button title="alert selected value" onPress={() => alert(ds)} color='pink'/>
       <KoroInput label='pipi' onChange={(text) => setDs(text)} />
-      <KoroSteps />
+      <KoroSteps 
+        steps={['im step number 1', 'im step number 2', 'im step number 3', 'im step number 4']}
+      />
     </View>
   );
 }

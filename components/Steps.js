754946d9c2fbfb328355694f48e2297bdc088004
Wisam <wisam_mozalbat@hotmail.com>
2020-03-23 21:48:46 -0400

    Steps update

--- components/Steps.js
+++ components/Steps.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+import { KoroButton } from './Button';
+
 export const KoroSteps = (props) => {
     let { steps = null } = props;
 
     let stepsArray = null;
+
+    let foregroundStyle = {}
+
+    
 
     if(Array.isArray(steps)){
         stepsArray = steps;
@@ -12,15 +18,28 @@ export const KoroSteps = (props) => {
 
     return (
         <View style={styles.container}>
-            <View style={styles.bar}></View>
-            <View style={styles.step}>
-                <Text>Step 1</Text>
+            <View style={styles.top}>
+                <View style={{...styles.barBackground}}></View>
+                <View style={{...styles.barForeground}}></View>
+                <View style={styles.step}>
+                    <Text>1</Text>
+                </View>
+                <View style={styles.step}>
+                    <Text>2</Text>
+                </View>
+                <View style={styles.step}>
+                    <Text>3</Text>
+                </View>
+                <View style={styles.step}>
+                    <Text>4</Text>
+                </View>
             </View>
-            <View style={styles.step}>
-                <Text>Step 2</Text>
+            <View style={styles.middle}>
+                <Text>Im the content</Text>
             </View>
-            <View style={styles.step}>
-                <Text>Step 3</Text>
+            <View style={styles.bottom}>
+                <Text>Im the bottom</Text>
+                <KoroButton title='continue'/>
             </View>
         </View>
     )
@@ -29,17 +48,54 @@ export const KoroSteps = (props) => {
 
 const styles = StyleSheet.create({
     container: {
-        flexDirection: 'row'
+        width: '80%',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    top: {
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    middle: {
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    bottom: {
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-end'
     },
     step: {
-        backgroundColor: 'grey',
+        zIndex: 150,
+        borderWidth: 1,
+        borderColor: 'rgba(0,0,0,0.5)',
+        backgroundColor: 'lightgrey',
         margin: 5,
         padding: 5,
         borderRadius: 50
     },
-    bar: {
-        backgroundColor: "black",
-        width: 10,
-        height: 10
+    barBackground: {
+        position: 'absolute',
+        top: '50%',
+        width: '90%',
+        height: 5,
+        marginHorizontal: 10,
+        zIndex: 100,
+        backgroundColor: "black"
+    },
+    barForeground: {
+        position: 'absolute',
+        top: '50%',
+        width: '0%',
+        height: 5,
+        marginHorizontal: 10,
+        zIndex: 105,
+        backgroundColor: "grey"
     }
 })

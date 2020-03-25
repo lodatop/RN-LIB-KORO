e34ca30179c03d8f4ba687cb360d48877dc148a5
Wisam <wisam_mozalbat@hotmail.com>
2020-03-24 22:34:08 -0400

    Popover donee

--- components/Popover.js
+++ components/Popover.js
@@ -1,44 +1,111 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, Modal, Dimensions, ScrollView, Text } from 'react-native';
 
 export const KoroPopover = (props) =>{
 
-    let { onRequestClose, animationType, visible, borderStyle, contentStyle, transparent = true, message } = props;
+    let { onRequestClose, animationType, borderStyle, contentStyle, transparent = true } = props;
     if (!animationType) animationType = 'slide';
+    let {position = 'topCenter'} = props;
+    const [visible, setVisible] = useState(props.visible);
+    const [title, setTitle] = useState(props.title);
+    const [content, setContent] = useState(props.content);
+    let popover = null
+    // const [popover, setPopover] = useState(null);
+
+
+    useEffect(()=>{setVisible(props.visible)},[props.visible])
+    useEffect(()=>{setTitle(props.title)},[props.title])
+    useEffect(()=>{setContent(props.content)},[props.content])
+
+    const positions = {
+        topCenter: {
+            containerStyle:{
+                position: 'absolute',
+                bottom: '110%'
+            },
+            arrowStyle: {
+                position: 'absolute',
+                bottom: -7,
+                alignSelf: 'center',
+            }
+        },
+        bottomCenter: {
+            containerStyle:{
+                position: 'absolute',
+                top: '110%'
+            },
+            arrowStyle: {
+                position: 'absolute',
+                top: -7,
+                alignSelf: 'center',
+            }
+        }
+    }
+
+    if(visible){
+       popover = (
+            <View 
+            style={{...styles.container, ...positions[position].containerStyle}}>
+                <View style={{...styles.arrow, ...positions[position].arrowStyle}}></View>
+                <View style={{...styles.popover}}>
+                    <Text style={{paddingBottom: 5, margin: 0, marginBottom: 5, borderBottomColor: 'lightgrey', borderBottomWidth: 1}}>
+                        {title}
+                    </Text>
+                    <Text>
+                        {content} 
+                    </Text>
+                </View>
+            </View>
+        )
+    }
 
     const onRequestCloseHandler = () => {
         if (onRequestClose) onRequestClose()
     }
 
     return (
-        <View style={[styles.backdrop]} visible={visible}>
-            <Modal {...props} transparent={transparent} animationType={animationType} visible={visible} onRequestClose={onRequestCloseHandler} animationType={animationType}>
-                <View style={{...styles.borderStyle, ...borderStyle}}>
-                    <View style={{...styles.contentStyle, ...contentStyle}}>
-                        <Text>{message}</Text>
-                        {props.children}
-                    </View>
-                </View>
-            </Modal>
+        <View style={{...styles.contentStyle}}>
+            {popover}
+            {props.children}
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    backdrop: {
-        backgroundColor: 'rgba(0,0,0,0.6)'
-    },
-    borderStyle: {
-        width:'100%',
-        height:'100%',
-        padding: 20,
-        backgroundColor: 'rgba(0,0,0,0.3)'
-    },
     contentStyle: {
-        backgroundColor: 'white', 
-        flex: 1,
-        padding: 15,
+        zIndex: 100,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+    },
+    popover: {
+        zIndex: 200,
+        backgroundColor: '#f5f5f5',
+        paddingHorizontal: 30,
+        paddingVertical: 10,
+        borderColor: '#adadad',
+        borderWidth: 1,
+        position: 'relative',
+        bottom: 0,
+        alignSelf: 'center',
+        elevation: 10
+    },
+    arrow: {
+        transform: [
+            { rotate: "45deg" }
+          ],
+        zIndex: 199,
+        
+        height: 16,
+        width: 16,
+        backgroundColor: '#adadad',
+        borderColor: 'black',
+        borderWidth: 1,
+    },
+    container: {
+        zIndex: 200,
+        maxWidth: 250,
+        flexWrap: 'wrap',
+        backgroundColor: 'transparent',
+        padding: 0
     }
 })
